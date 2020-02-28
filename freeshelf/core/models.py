bd15a7447d0284b3973ba899c27c11e18fc8bda6
MFvanRossum <matissev@hotmail.com>
2020-02-28 12:40:57 -0500

    Creates Book model and sets up basic HTML

--- freeshelf/core/models.py
+++ freeshelf/core/models.py
@@ -2,4 +2,11 @@ from django.db import models
 from django.utils.text import slugify
 
 class Book(models.Model):
-   pass 
+   title = models.CharField(max_length=80)
+   author = models.CharField(max_length=30)
+   description = models.TextField(max_length=None)
+   date_added = models.DateTimeField(auto_now_add=True)
+   url = models.URLField(max_length=200)
+   
+   def __str__(self):
+        return f"Title: {self.title} Author: {self.author}"
