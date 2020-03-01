492dcd9cd488c90ffe329c35c6c88aac7ffca4c9
MFvanRossum <matissev@hotmail.com>
2020-03-01 15:36:40 -0500

    Adds an image field to the admin form

--- freeshelf/freeshelf/settings.py
+++ freeshelf/freeshelf/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
+MEDIA_URL = '/media/'
--- freeshelf/freeshelf/urls.py
+++ freeshelf/freeshelf/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.books_list, name = 'books-list'),
     path('books/<int:pk>/', views.books_detail, name = 'books-detail'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
